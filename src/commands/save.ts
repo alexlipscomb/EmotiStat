4941551ac8aa005066eecc5cf564fa1275161e95
Alex Lipscomb <johnalipscomb@gmail.com>
2022-03-28 04:03:52 -0500

    Added statuscode checking

--- src/commands/save.ts
+++ src/commands/save.ts
@@ -1,6 +1,6 @@
 import archiver from 'archiver';
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, GuildEmoji } from 'discord.js';
+import { CommandInteraction, GuildEmoji, Permissions } from 'discord.js';
 import fetch from 'node-fetch';
 import fs from 'fs';
 import path from 'path';
@@ -13,6 +13,11 @@ export const data = new SlashCommandBuilder()
     .setDescription('Saves the emotes on the server to a .zip');
 
 export async function execute(interaction: CommandInteraction): Promise<void> {
+    if (!interaction.memberPermissions?.has(Permissions.FLAGS.ADMINISTRATOR)) {
+        interaction.reply('You need to be an administrator to use this command.');
+        return;
+    }
+
     const { guild } = interaction;
 
     if (guild === undefined) {
@@ -36,25 +41,37 @@ export async function execute(interaction: CommandInteraction): Promise<void> {
     await fs.promises.mkdir(serverPath, { recursive: true });
 
     const throttle = pThrottle({
-        limit: 1,
+        limit: 2,
         interval: 100,
     });
 
-    const throttled = throttle(async (emoji: GuildEmoji) => {
+    const throttled = throttle(async (emoji: GuildEmoji): Promise<number> => {
         const { name, url, id } = emoji;
 
-        logger.debug(`Saving: ${name}-${id}-${url.split('.').pop()}`);
         const emojiFilename = `${name}-${id}-${url.split('.').pop()}`;
+
+        logger.debug(url);
 
         const res = await fetch(url);
 
-        const emojiPath = path.join(serverPath, emojiFilename);
-        res.body?.pipe(fs.createWriteStream(emojiPath));
+        if (res.status !== 200) {
+            logger.warn(`Failed to get HTTP 200 Response: ${name}-${id}-${url.split('.').pop()} RESPONSE: ${res.status}`);
+            return 0;
+        }
 
-        return emojiPath;
+        logger.debug(`Saving: ${name}-${id}-${url.split('.').pop()}`);
+        const emojiPath = path.join(serverPath, emojiFilename);
+
+        const dest = fs.createWriteStream(emojiPath);
+        dest.on('error', (err) => {
+            logger.error(err);
+        });
+
+        res.body?.pipe(dest);
+        return 1;
     });
 
-    await Promise.all(
+    const savedEmotes = await Promise.all(
         emojis!.map((emoji) => throttled(emoji)),
     );
 
@@ -73,8 +90,18 @@ export async function execute(interaction: CommandInteraction): Promise<void> {
 
     await archive.finalize();
 
+    const numSaved = savedEmotes.reduce((acc, curr) => acc + curr, 0);
+
+    let content = '';
+
+    if (numSaved === emojis!.size) {
+        content = 'All emojis were saved successfully!\nDownload the zip below:'; // TODO add list of unsaved emojis
+    } else {
+        content = `${numSaved} of ${emojis!.size} emojis were saved successfully!\nDownload the zip below:`;
+    }
+
     await interaction.editReply({
-        content: `Saved ${emojis!.size} emojis! Download the zip below:`,
+        content,
         files: [{
             attachment: path.join(serverPath, 'emoji.zip'),
             name: 'emoji.zip',
